90403053b0a97d0c3284b1aec41b0c0fa0b9deae
Denilson Behenck <denilsontrespa10@gmail.com>
2022-08-05 00:04:59 -0300

    create to form to valided

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,12 +13,35 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleForValidationScheme = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser do mínimo 05 minutos')
+    .max(60, 'O ciclo precisa ser do máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleForValidationScheme>
+
 export function Home() {
-  const [task, setTask] = useState()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleForValidationScheme),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCircle(data) {
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCircle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -23,6 +49,7 @@ export function Home() {
             type="text"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -38,8 +65,7 @@ export function Home() {
             type="number"
             placeholder="00"
             step={5}
-            min={5}
-            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -53,7 +79,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCoutdownButton type="submit">
+        <StartCoutdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCoutdownButton>
